50b8cc6f787efcbbfffb5365dbae826c41543f46
Hanz <hanz.diener@gmail.com>
2024-07-04 22:24:29 -0500

    Tabla Notificaciones.cs + NotificacionesDAO

---
+++ Panaderia_AccesoDatos/DAO/NotificacionesDAO.cs
@@ -0,0 +1,128 @@
+﻿using Panaderia_AccesoDatos.Entidades;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Panaderia_AccesoDatos.DAO
+{
+    public class NotificacionesDAO
+    {
+        private ConexionBD conexion = new ConexionBD();
+
+        // Listar notificaciones sin usar SP
+        public DataTable ListarNotificacionesSinSP()
+        {
+            DataTable dt = new DataTable();
+            SqlCommand ejecutarSql = new SqlCommand();
+            SqlDataReader transaccion = null;
+
+            try
+            {
+                ejecutarSql.Connection = conexion.AbrirConexion();
+
+                ejecutarSql.CommandText = "SELECT ID_notificacion, ID_pedido, Tipo, Fecha_Notificacion FROM Notificaciones";
+                transaccion = ejecutarSql.ExecuteReader();
+                dt.Load(transaccion);
+                conexion.CerrarConexion();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al listar Notificacion: " + ex.Message);
+            }
+            finally
+            {
+                if (transaccion != null)
+                {
+                    transaccion.Close();
+                }
+                conexion.CerrarConexion();
+            }
+        }
+
+        // Listar notificaciones utilizando procedimiento almacenado
+        public DataTable ListarNotificaciones()
+        {
+            DataTable dt = new DataTable();
+            SqlCommand ejecutarSql = new SqlCommand();
+            SqlDataReader transaccion = null;
+
+            try
+            {
+                ejecutarSql.Connection = conexion.AbrirConexion();
+
+                ejecutarSql.CommandText = "pr_ListarNotificaciones";
+                ejecutarSql.CommandType = CommandType.StoredProcedure;
+                transaccion = ejecutarSql.ExecuteReader();
+                dt.Load(transaccion);
+                conexion.CerrarConexion();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al listar Notificacion: " + ex.Message);
+            }
+            finally
+            {
+                if (transaccion != null)
+                {
+                    transaccion.Close();
+                }
+                conexion.CerrarConexion();
+            }
+        }
+
+        // Insertar notificación utilizando procedimiento almacenado
+        public void InsertarNotificacion(Notificaciones nuevaNotificacion)
+        {
+            SqlCommand ejecutarSql = new SqlCommand();
+
+            try
+            {
+                ejecutarSql.Connection = conexion.AbrirConexion();
+
+                ejecutarSql.CommandText = "pr_InsertarNotificacion";
+                ejecutarSql.CommandType = CommandType.StoredProcedure;
+                ejecutarSql.Parameters.AddWithValue("@ID_notificacion", nuevaNotificacion.ID_Notificaciones);
+                ejecutarSql.Parameters.AddWithValue("@ID_pedido", nuevaNotificacion.ID_pedido);
+                ejecutarSql.Parameters.AddWithValue("@Tipo", nuevaNotificacion.Tipo);
+                ejecutarSql.Parameters.AddWithValue("@Fecha_Notificacion", nuevaNotificacion.Fecha_Notificacion);
+                ejecutarSql.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al insertar notificación: " + ex.Message);
+            }
+            finally
+            {
+                conexion.CerrarConexion();
+            }
+        }
+
+        // Insertar notificación sin usar SP
+        public void InsertarNotificacionesSinSP(Notificaciones nuevaNotificacion)
+        {
+            SqlCommand ejecutarSql = new SqlCommand();
+
+            try
+            {
+                ejecutarSql.Connection = conexion.AbrirConexion();
+
+                ejecutarSql.CommandText = "INSERT INTO Notificaciones (ID_notificacion, ID_pedido, Tipo, Fecha_Notificacion) VALUES (@ID_notificacion, @ID_pedido, @Tipo, @Fecha_Notificacion)";
+                ejecutarSql.Parameters.AddWithValue("@ID_notificacion", nuevaNotificacion.ID_Notificaciones);
+                ejecutarSql.Parameters.AddWithValue("@ID_pedido", nuevaNotificacion.ID_pedido);
+                ejecutarSql.Parameters.AddWithValue("@Tipo", nuevaNotificacion.Tipo);
+                ejecutarSql.Parameters.AddWithValue("@Fecha_Notificacion", nuevaNotificacion.Fecha_Notificacion);
+                ejecutarSql.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al insertar notificación: " + ex.Message);
+            }
+            finally
+            {
+                conexion.CerrarConexion();
+            }
+        }
+    }
+}
---
+++ Panaderia_AccesoDatos/Entidades/Notificaciones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panaderia_AccesoDatos.Entidades
+{
+    public class Notificaciones
+    {
+        public int ID_Notificaciones { get; set; }
+        public int ID_pedido { get; set; }
+        public string Tipo { get; set; }
+        public DateTime Fecha_Notificacion { get; set; }
+    }
+}
