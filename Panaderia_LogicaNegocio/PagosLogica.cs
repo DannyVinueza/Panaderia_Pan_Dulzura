3cf18bd6c9c7460b73bd0a0a4eff6ff699e1f2bb
JenniferEstefania <e1722415195@uisrael.edu.ec>
2024-07-12 03:29:12 -0500

    Add files via upload

---
+++ Panaderia_LogicaNegocio/PagosLogica.cs
@@ -0,0 +1,51 @@
+﻿using Panaderia_AccesoDatos.DAO;
+using Panaderia_AccesoDatos.Entidades;
+using System;
+using System.Data;
+
+namespace Panaderia_LogicaNegocio
+{
+    public class PagosLogica
+    {
+        private PagosDAO PagosDAO;
+        public PagosLogica()
+        {
+            PagosDAO = new PagosDAO();
+        }
+
+        //Metodo para insertar un pago
+
+        public bool InsertarPago(Pagos nuevoPago)
+        {
+            try
+            {
+                PagosDAO.InsertarPago(nuevoPago);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error: " + ex.Message);
+
+            }
+        }
+        //Metodo para listar un pago
+        public DataTable ListarPagos()
+        {
+            try
+            {
+                return PagosDAO.ListarPagos();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error: " + ex.Message);
+
+
+
+            }
+
+
+
+        }
+    }
+}
